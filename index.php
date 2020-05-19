f2595fee72c3abd00b02300007dbeb678a594db3
Vinc <vinc_944@hotmail.com>
2020-05-19 17:41:11 +0100

    Contactme Skeleton added

--- index.php
+++ index.php
@@ -4,33 +4,33 @@
 
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>PORTFOLIO</title>
+    <head>
+        <title>PORTFOLIO</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="css/normalize.css">
         <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
         <link rel="stylesheet" href="./css/style.css" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Spartan&display=swap" rel="stylesheet"/>
         <script src="./scripts/main.js" async></script>
-	</head>
-	<body>
-		<nav class="navbar d-inline-flex container-fluid">
+    </head>
+    <body>
+        <nav class="navbar d-inline-flex container-fluid">
             <div class="navbar-brand col-5">VP</div>
-			<div class="navbar- col-6 d-flex flex-row">
+            <div class="navbar- col-6 d-flex flex-row">
                 <div class="col-2 nav-item homeicon" id="homebtn">HOME</div>
                 <div class="col-2 nav-item" id="aboutmebtn">ABOUT ME</div>
                 <div class="col-2 nav-item portfolioicon" id="portfoliobtn">PORTFOLIO</div>
-                <div class="col-2 nav-item contacticon" id="contactmebtn">CONTACT ME</div>
-			</div>
-		</nav>
-		<div class="container justify-content-center align-items-center text-center" id = "home">
-			<div>
-				<h1>Hello There</h1>
-				<h3>Welcome to my world!</h3>
-				<p>This is my world created with html and css.</p>
-			</div>
-		</div>
+                <div class="col-2 nav-item contacticon" id="contactMebtn">CONTACT ME</div>
+            </div>
+        </nav>
+        <div class="container justify-content-center align-items-center text-center" id = "home">
+            <div>
+                <h1>Hello There</h1>
+                <h3>Welcome to my world!</h3>
+                <p>This is my world created with html and css.</p>
+            </div>
+        </div>
         <div class = "container justify-content-center flex-column text-center" id = "aboutMe">
             <div><h2>ABOUT ME</h2></div>
             <div class="aboutmecontainer d-flex flex-row">
@@ -40,7 +40,8 @@
                 <div class="contentcontainer d-flex col-6 align-items-center justify-content-center">
                     <div>
                         <div><h3>Vincent Pires</h3></div>
-                        <div><p>Hi I'm Vincent, an aspiring full stack developer currently training at Mayden Academy.</p></div>
+                        <div><p>Hi I'm Vincent, an aspiring full stack developer currently training at
+                                Mayden Academy.</p></div>
                     </div>
                 </div>
             </div>
@@ -77,27 +78,27 @@
                     </div>
             </div>
         </div>
-<!--		<div class="contactme" id="contactme">-->
-<!--			<h2>Contact Me</h2>-->
-<!--			<div class="aboutmecontainer">-->
-<!--				<div>-->
-<!--					<h3>Is this the dev you are looking for?</h3>-->
-<!--					<p>If so mail me</p>-->
-<!--					<a href="mailto:vinc_944@hotmail.com" class="email">EMAIL: vinc_944@hotmail.com</a>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--			<div class="social">-->
-<!--				<a href="https://twitter.com/vincdaman">-->
-<!--					<img src="Assets/twitter.png" alt="twitter" class="icon">-->
-<!--				</a>-->
-<!--				<a href="https://github.com/Sen-i">-->
-<!--					<img src="Assets/github.png" alt="github" class="icon">-->
-<!--				</a>-->
-<!--				<a href="https://www.facebook.com/Vincdaman">-->
-<!--					<img src="Assets/fbimg.png" alt="facebook" class="icon">-->
-<!--				</a>-->
-<!--			</div>-->
-<!--		</div>-->
-		<footer>&copy; Vincent 2020</footer>
-	</body>
+        <div class="contactme container justify-content-center flex-column text-center" id="contactMe">
+            <h2>Contact Me</h2>
+            <div class="aboutmecontainer">
+                <div>
+                    <h3>Is this the dev you are looking for?</h3>
+                    <p>If so mail me</p>
+                    <a href="mailto:vinc_944@hotmail.com" class="email">EMAIL: vinc_944@hotmail.com</a>
+                </div>
+            </div>
+            <div class="social">
+                <a href="https://twitter.com/vincdaman">
+                    <img src="Assets/twitter.png" alt="twitter" class="icon">
+                </a>
+                <a href="https://github.com/Sen-i">
+                    <img src="Assets/github.png" alt="github" class="icon">
+                </a>
+                <a href="https://www.facebook.com/Vincdaman">
+                    <img src="Assets/fbimg.png" alt="facebook" class="icon">
+                </a>
+            </div>
+        </div>
+        <footer>&copy; Vincent 2020</footer>
+    </body>
 </html>
--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,49 @@
-function hideScreen(cssTag) {
-    console.log(cssTag);
+function hideScreen(cssTag)
+{
     document.querySelector(cssTag).style.display = 'none';
 }
 
-function makeScreenFlex(cssTag) {
-    console.log(cssTag);
+function makeScreenFlex(cssTag)
+{
     document.querySelector(cssTag).style.display = 'flex';
 }
 
-function showHome() {
+function showHome()
+{
 
     hideScreen('#aboutMe');
     hideScreen('#portfolio');
-    // hideScreen('#portfolio');
+    hideScreen('#contactMe');
     makeScreenFlex('#home');
 
 }
 
-function showAboutMe() {
-console.log(1);
+function showAboutMe()
+{
+
     makeScreenFlex('#aboutMe');
     hideScreen('#portfolio');
-    // hideScreen('#portfolio');
+    hideScreen('#contactMe');
     hideScreen('#home');
 
 }
 
-function showPortfolio() {
+function showPortfolio()
+{
 
     hideScreen('#aboutMe');
     makeScreenFlex('#portfolio');
-    // hideScreen('#portfolio');
+    hideScreen('#contactMe');
+    hideScreen('#home');
+
+}
+
+function showContactMe()
+{
+
+    hideScreen('#aboutMe');
+    makeScreenFlex('#contactMe');
+    hideScreen('#portfolio');
     hideScreen('#home');
 
 }
@@ -39,3 +52,4 @@ function showPortfolio() {
 document.querySelector('#homebtn').addEventListener('click', showHome);
 document.querySelector('#aboutmebtn').addEventListener('click', showAboutMe);
 document.querySelector('#portfoliobtn').addEventListener('click', showPortfolio);
+document.querySelector('#contactMebtn').addEventListener('click', showContactMe);
